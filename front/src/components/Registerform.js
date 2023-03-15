69f55e02e3c3a2d1b068a09055d7a137bb48c1c4
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2023-03-15 17:18:03 +0100

    form created waiting db

--- front/src/components/Registerform.js
+++ front/src/components/Registerform.js
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 const Registerform = ({ db }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     db.query(
-      `INSERT INTO users (username, password) VALUES (?, ?)`,
-      [username, password],
+      `INSERT INTO users (username, password, email) VALUES (?, ?)`,
+      [username, password, email],
       (err) => {
         if (err) {
           console.error("Error inserting user:", err);
@@ -18,15 +19,45 @@ const Registerform = ({ db }) => {
         }
       }
     );
+
+    const value = event.target.value;
+    const regex = /^\S+@\S+\.\S+$/;
+
+    if (regex.test(value)) {
+      setEmail(value);
+    } else {
+      alert("Adresse mail invalide !");
+      setEmail(alert);
+    }
   };
 
+  //   Pour vérifier que le format de l'email est correct
+  //   function handleEmailChange(event) {
+  //     const value = event.target.value;
+  //     const regex = /^\S+@\S+\.\S+$/;
+
+  //     if (regex.test(value)) {
+  //       setMail(value);
+  //     } else {
+  //       alert("Adresse mail invalide !");
+  //       setMail(alert);
+  //     }
+  //   }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="formRegister" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Pseudo"
         value={username}
         onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Adresse mail"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        // onSubmit={handleEmailChange}
       />
       <input
         type="password"
