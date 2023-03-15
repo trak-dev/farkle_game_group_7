6a1f29cbc8907f576246cd7fa864507d95cbc309
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2023-03-15 15:25:33 +0100

    architec install

---
+++ front/src/pages/ladder.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Ladder = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>CLASSEMENT</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        sapiente explicabo optio, nisi repellendus mollitia non ipsa, debitis
+        fugiat, maiores id dicta adipisci voluptatem reiciendis neque tenetur
+        aut? Nostrum, corrupti.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+        distinctio consequatur vero id cum porro assumenda natus omnis corrupti
+        expedita cumque, sit voluptatem, accusamus eos animi fugit repellendus
+        at. Non et vel quidem fugit harum quibusdam unde laudantium, rerum
+        placeat esse recusandae, eligendi tempore quia sit fugiat consequuntur
+        quas natus veritatis. Neque dolor libero nisi illum molestias nostrum
+        corporis ipsam quibusdam, ea itaque corrupti, aperiam aut sed assumenda
+        quidem nemo magni voluptatem incidunt ipsa ex! Id eveniet distinctio
+        enim praesentium voluptate amet dolorum molestiae libero! Deserunt et
+        esse illo impedit modi aliquid, repellendus reiciendis, quaerat incidunt
+        saepe, ipsa illum totam!
+      </p>
+    </div>
+  );
+};
+
+export default Ladder;
